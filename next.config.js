0dbb1eacc7ebf664146c1acd56a804e62fb8f672
dianazawislak <diana.zawislak@icloud.com>
2023-12-12 17:36:33 -0500

    config updates

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    appDir: true,
- 
+    
   },
   images: {
     domains: ["links.papareact.com", "cdn.sanity.io", "cdn.discordapp.com"],
